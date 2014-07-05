084db26ac018a70d222d8ddc3ddd4bbec153b858
Lianyi Zhou <lianyzhou@gmail.com>
2014-07-05 21:45:37 +0800

    Update demo.js

--- demo7/scripts/demo.js
+++ demo7/scripts/demo.js
@@ -2,7 +2,7 @@ angular.module("webapp",[]);
 
 angular.module("webapp").controller("HttpCtrl", ["$scope","$http",function($scope,$http) {
 	
-	$http.get("/getbooks.php").success(function(ret) {
+	$http.get("/getbooks").success(function(ret) {
 		$scope.bookList = ret;
 	}).error(function(msg) {
 		alert(msg);
